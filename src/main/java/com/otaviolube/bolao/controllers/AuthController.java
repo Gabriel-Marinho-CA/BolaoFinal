b4a649a96b1bfe29a383bd5a2e67f1a39bccde68
Otávio Lube dos Santos <otaviolube@gmail.com>
2022-11-16 07:27:45 -0300

    Ajustando projeto

--- src/main/java/com/otaviolube/bolao/controllers/AuthController.java
+++ src/main/java/com/otaviolube/bolao/controllers/AuthController.java
@@ -1,20 +1,38 @@
 package com.otaviolube.bolao.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.otaviolube.bolao.models.UserModel;
+import com.otaviolube.bolao.models.UserRepository;
 
 @Controller
 public class AuthController {
     
     private Boolean isAuthenticated;
 
+    @Autowired
+    UserRepository userRepository;
+
     public AuthController() {
         this.isAuthenticated = false;
     }
 
+    @GetMapping(value = "/teste")
+    public String teste(Model model){
+        model.addAttribute("page", "dashboard");
+        return "index";
+    }
+
     @GetMapping(value = "/")
-    public String index(){
+    public String index(Model model){
         if(this.isAuthenticated){
+            model.addAttribute("page", "dashboard");
             return "index";
         }else{
             return "login";
@@ -22,12 +40,17 @@ public class AuthController {
     }
 
     @GetMapping(value = "/register")
-    public String register(){
-        if(this.isAuthenticated){
-            return "index";
-        }else{
-            return "register";
+    public String register(UserModel userModel){
+        return "register";
+    }
+
+    @PostMapping(value = "/adduser")
+    public String registerUser(@Validated UserModel user, BindingResult result, Model model){
+        if(result.hasErrors()){
+            return "redirect:/register";
         }
+        userRepository.save(user);
+        return "redirect:/login";
     }
 
     @GetMapping(value = "/login")
@@ -47,7 +70,4 @@ public class AuthController {
             return "forgot-password";
         }
     }
-
-    
-
 }
